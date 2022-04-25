b60c5dd9dd785e1b50150262ecf99acb8eae1271
Nguyễn Trọng Nhân <nhantrong3123@gmail.com>
2022-04-25 23:32:22 +0700

    loginApi 30%...

--- app/src/main/java/com/nhom12/rau_cu_xanh/api/login/login_Api.kt
+++ app/src/main/java/com/nhom12/rau_cu_xanh/api/login/login_Api.kt
@@ -1,4 +1,35 @@
 package com.nhom12.rau_cu_xanh.api.login
 
+import com.nhom12.rau_cu_xanh.model.user
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
 interface login_Api {
+
+    companion object {
+
+        var BASE_URL = "192.168.0.101"
+
+        fun create() : login_Api {
+
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build()
+            return retrofit.create(login_Api::class.java)
+
+        }
+    }
+
+//
+//    @GET("volley_array.json")
+//    fun getMovies() : Call<List<Movie>>
+
+    @GET("api/v1/resources/books")
+    fun getMovies() : Call<List<user>>
 }
+
+
+
---
+++ app/src/main/java/com/nhom12/rau_cu_xanh/model/user.kt
@@ -0,0 +1,4 @@
+package com.nhom12.rau_cu_xanh.model
+
+class user {
+}
